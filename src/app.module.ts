267adc4937bd95a8b1903bab0ae160ae82ad2c4f
EndlessTJ <1294546949@qq.com>
2020-03-23 18:49:58 +0800

    delete code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { ArticleModule } from './Article/article.module'
-import { AppService } from './app.service';
+import { ArticleModule } from './Article/article.module';
 import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost/SIMBOL_BLOG'), ArticleModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
